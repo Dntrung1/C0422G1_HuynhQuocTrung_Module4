0d02207aae48f6376fc44af1fedba5dc15d88969
dntrung1 <dntrung1@gmail.com>
2022-09-06 17:03:49 +0700

    update

--- ss13_Spring_Secirity/exercise/Blog/src/main/java/com/codegym/controller/BlogController.java
+++ ss13_Spring_Secirity/exercise/Blog/src/main/java/com/codegym/controller/BlogController.java
@@ -6,22 +6,16 @@ import com.codegym.service.IBlogService;
 import com.codegym.service.ICategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.List;
-import java.util.Optional;
-
 @Controller
+@RequestMapping("/blog")
 public class BlogController {
     @Autowired
     private IBlogService iBlogService;
--- ss13_Spring_Secirity/exercise/Blog/src/main/java/com/codegym/repository/IBlogRepository.java
+++ ss13_Spring_Secirity/exercise/Blog/src/main/java/com/codegym/repository/IBlogRepository.java
@@ -5,9 +5,6 @@ import com.codegym.model.Category;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import java.util.List;
 
 public interface IBlogRepository extends JpaRepository<Blog, Integer> {
     Page<Blog> findAllByDescriptionContaining(Pageable pageable, String search);
